eaec6b9d3148a3aca95dce87449ef2db1fe6129c
Jonathan Lam <jonathanlam@Jonathans-MBP.lan>
2021-07-20 01:33:07 -0400

    initial login screen

--- app/src/main/java/com/example/beesmartthermostat/MainActivity.java
+++ app/src/main/java/com/example/beesmartthermostat/MainActivity.java
@@ -2,11 +2,14 @@ package com.example.beesmartthermostat;
 
 import android.os.Bundle;
 import android.support.wearable.activity.WearableActivity;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends WearableActivity {
 
     private TextView mTextView;
+    private EditText editText_email;
+    private EditText editText_password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
